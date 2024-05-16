a070bfd6af8635ada0d09c54368190cab37f7358
Tadej Jeršin <104827264+tadejjersin@users.noreply.github.com>
2024-05-16 10:02:31 +0200

    dodal klice za ustvarjanje tabel

---
+++ Data/create_db.sql
@@ -0,0 +1,35 @@
+CREATE TABLE Uporabnik(
+    uporabnisko_ime TEXT PRIMARY KEY,
+    ime TEXT NOT NULL,
+    priimek TEXT NOT NULL,
+    geslo TEXT NOT NULL 
+);
+
+CREATE TABLE Portfelj(
+    id SERIAL PRIMARY KEY,
+    ime TEXT NOT NULL,
+    lastnik TEXT NOT NULL REFERENCES Uporabnik(uporabnisko_ime)
+);
+
+CREATE TABLE Sredstvo(
+    id SERIAL PRIMARY KEY,
+    kratica TEXT NOT NULL,
+    tip TEXT NOT NULL,
+    ime TEXT NOT NULL,
+    zadnja_cena FLOAT NOT NULL
+);
+
+CREATE TABLE Transakcija(
+    id SERIAL PRIMARY KEY,
+    kolicina FLOAT NOT NULL,
+    vrednost FLOAT NOT NULL,
+    cas DATETIME NOT NULL,
+    portfelj INTEGER NOT NULL REFERENCES Portfelj(id),
+    sredstvo INTEGER NOT NULL REFERENCES Sredstvo(id)
+);
+
+CREATE TABLE ZgodovinaCen(
+    sredstvo INTEGER NOT NULL REFERENCES Sredstvo(id),
+    cas DATETIME NOT NULL,
+    cena FLOAT NOT NULL
+);
