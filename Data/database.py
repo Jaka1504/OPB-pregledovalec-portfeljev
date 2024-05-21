a6f7aa61ad677e0321208d6c13b94c61e0cda6f7
Tadej Jeršin <104827264+tadejjersin@users.noreply.github.com>
2024-05-21 22:14:10 +0200

    dodal še nekaj osnovnih funkcij za delo z bazo

--- Data/database.py
+++ Data/database.py
@@ -1,12 +1,12 @@
 import psycopg2, psycopg2.extensions, psycopg2.extras
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
 
-from Data.modeli import *
-import Data.auth_public as auth
+from modeli import *
+import auth_public as auth
 
 
 
-DB_PORT = os.environ.get('POSTGRES_PORT', 5432)
+DB_PORT = 5432
 
 class Repo:
     def __init__(self): 
@@ -53,4 +53,114 @@ class Repo:
         data = (transakcija.kolicina, transakcija.vrednost, transakcija.cas, transakcija.portfelj, transakcija.sredstvo)
         self.cur.execute(cmd, data)
         transakcija.id = self.cur.fetchone()[0]
+        self.conn.commit()
+
+    def dobi_uporabnike(self):
+        cmd = """
+            SELECT uporabnisko_ime, geslo, ime, priimek from uporabnik
+            """
+        self.cur.execute(cmd)
+        uporabniki = [Uporabnik.from_dict(u) for u in self.cur.fetchall()]
+        return uporabniki
+
+    def dobi_uporabnika(self, uporabnisko_ime):
+        cmd = f"""
+            SELECT uporabnisko_ime, geslo, ime, priimek from uporabnik
+            WHERE uporabnisko_ime = '{uporabnisko_ime}'
+            """
+        self.cur.execute(cmd)
+        uporabnik = Uporabnik.from_dict(self.cur.fetchone())
+        return uporabnik
+
+    def dobi_uporabnikove_portfelje(self, uporabnisko_ime):
+        cmd = f"""
+            SELECT id, ime, lastnik from portfelj 
+            WHERE lastnik = '{uporabnisko_ime}'
+            """
+        self.cur.execute(cmd)
+        portfelji = [Portfelj.from_dict(p) for p in self.cur.fetchall()]
+        return portfelji
+
+    def dobi_portfelj(self, id):
+        cmd = f"""
+            SELECT id, ime, lastnik from portfelj
+            WHERE id = {id}
+            """
+        self.cur.execute(cmd)
+        portfelj = Portfelj.from_dict(self.cur.fetchone())
+        return portfelj
+    
+    def dobi_transakcijo(self, id):
+        cmd = f"""
+            SELECT id, kolicina, vrednost, cas, portfelj, sredstvo from transakcija
+            WHERE id = {id}
+            """
+        self.cur.execute(cmd)
+        transakcija = Transakcija.from_dict(self.cur.fetchone())
+
+    def dobi_transakcije_v_portfelju(self, id, sredstvo=None):
+        cmd = f"""
+            SELECT id, kolicina, vrednost, cas, portfelj, sredstvo from transakcija
+            WHERE portfelj = {id} 
+            """
+        if sredstvo is not None:
+            cmd += f"""
+                AND sredstvo = {sredstvo}    
+                """
+        self.cur.execute(cmd)
+        transakcije = [Transakcija.from_dict(t) for t in self.cur.fetchall()]
+        return transakcije
+
+    def dobi_uporabnikove_transakcije(self, uporabnisko_ime, sredstvo=None):
+        cmd = f"""
+            SELECT t.id, t.kolicina, t.vrednost, t.cas, t.portfelj, t.sredstvo 
+            from transakcija t
+            JOIN portfelj ON t.portfelj = portfelj.id
+            JOIN uporabnik ON lastnik = uporabnik.uporabnisko_ime 
+            WHERE lastnik = '{uporabnisko_ime}'
+            """
+        if sredstvo is not None:
+            cmd += f"""
+                AND sredstvo = {sredstvo}    
+                """
+        self.cur.execute(cmd)
+        transakcije = [Transakcija.from_dict(t) for t in self.cur.fetchall()]
+        return transakcije
+
+    def dobi_sredstvo(self, id):
+        cmd = f"""
+            SELECT id, kratica, tip, ime, zadnja_cena from sredstvo
+            WHERE id = {id}
+            """
+        self.cur.execute(cmd)
+        sredstvo = Sredstvo.from_dict(self.cur.fetchone())
+        return sredstvo
+
+    def dobi_sredstva(self):
+        cmd = f"""
+            SELECT id, kratica, tip, ime, zadnja_cena from sredstvo
+            """
+        self.cur.execute(cmd)
+        sredstva = [Sredstvo.from_dict(s) for s in self.cur.fetchall()]
+        return sredstva
+
+    def dobi_kolicino_sredstev_v_portfelju(self, id):
+        cmd = f"""
+            SELECT sredstvo, sum(t.kolicina)
+            FROM transakcija t
+            JOIN portfelj p ON t.portfelj = p.id
+            GROUP BY sredstvo, p.id
+            HAVING p.id = {id}
+            """
+        self.cur.execute(cmd)
+        kolicine = dict(self.cur.fetchall())
+        return kolicine
+
+    def posodobi_ceno_sredstva(self, id, cena):
+        cmd = f"""
+            UPDATE sredstvo
+            SET zadnja_cena = {cena}
+            WHERE id = {id}
+            """
+        self.cur.execute(cmd)
         self.conn.commit()
