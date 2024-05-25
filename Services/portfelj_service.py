e264a0328790be60f6ca5c42876872ca521e512e
Tadej Jeršin <104827264+tadejjersin@users.noreply.github.com>
2024-05-25 23:18:52 +0200

    napisal funkcije za posodabljanje portfeljev in računanje njihovih vrednosti

--- Services/portfelj_service.py
+++ Services/portfelj_service.py
@@ -1,15 +1,50 @@
 from Data.database import Repo
 from Data.modeli import *
+from Data.api import Api
 
-#TODO
+
 class PortfeljService():
-    def __init__():
+    def __init__(self):
         self.repo = Repo()
+        self.api = Api()
 
-    def posodobi_portfelj(self, id):
-        '''Dobi trenutne cene kriptovalut v portfelju z id-jem id, jim posodobi cene, izračuna vrednost 
-        portfelja ter jo vrne in vstavi ustrezne podatke v tabeli VrednostPortfelja in CenaKriptovalute.'''
-        pass
+    def posodobi_portfelje(self, ids : list):
+        '''Dobi trenutne cene kriptovalut v portfeljih z id-ji v seznamu ids, jim posodobi cene, izračuna vrednost 
+        portfeljev ter jih vrne in vstavi ustrezne podatke v tabeli VrednostPortfelja in CenaKriptovalute.'''
+        portfelji = [self.repo.dobi_portfelj(id) for id in ids]
+        seznam_kolicin = [self.repo.dobi_kolicino_kriptovalut_v_portfelju(id) for id in ids]
+        seznam_kriptovalut = list(set(sum([list(d.keys()) for d in seznam_kolicin], [])))
+        kriptovalute, cas = self.api.dobi_cene_kriptovalut(seznam_kriptovalut)
+        vrednosti = [0 for _ in portfelji]
+        for kripto in kriptovalute:
+            kolicine = [kolicine_dict.get(kripto.id, 0) for kolicine_dict in seznam_kolicin]
+            cena = kripto.zadnja_cena
+            for i, kolicina in enumerate(kolicine):
+                vrednosti[i] += kolicina * cena
+            
+            cenaKriptovalute = CenaKriptovalute(
+                kriptovaluta = kripto.id,
+                cas = cas,
+                cena = kripto.zadnja_cena
+            )
+            self.repo.posodobi_ceno_kriptovalute(kripto.id, kripto.zadnja_cena)
+            self.repo.dodaj_ceno_kriptovalute(cenaKriptovalute)
+
+        vrednostiPortfeljev = [VrednostPortfelja(
+            portfelj = portfelj.id,
+            cas = cas,
+            vrednost = round(vrednosti[i], 2)
+        ) for i, portfelj in enumerate(portfelji)]
+        for vp in vrednostiPortfeljev:
+            self.repo.dodaj_vrednost_porfelja(vp)
+
+        return [PortfeljDto(
+            id = portfelj.id,
+            lastnik = portfelj.lastnik,
+            ime = portfelj.ime,
+            vrednost = round(vrednosti[i], 2)
+        ) for i, portfelj in enumerate(portfelji)]
+
 
     def najdi_vse_portfelje(self, uporabnisko_ime):
         '''Vrne seznam portfeljev uporabnika. Vsak portfelj je slovar oblike
@@ -21,7 +56,10 @@ class PortfeljService():
             "donos" : 101.01,           # razlika prejsnjih dveh
             "trend" : 0.4               # za koliko procentov se je povecala vrednost, če pogledamo zadnja dva (razlicna?) vnosa
         }'''
-        return self.repo.dobi_uporabnikove_portfelje(uporabnisko_ime)
+        portfelji = self.repo.dobi_uporabnikove_portfelje(uporabnisko_ime)
+        ids = [p.id for p in portfelji]
+        portfeljiDto = self.posodobi_portfelje(ids)
+        return portfeljiDto
 
 
     def najdi_portfelj(self, id_portfelja):
@@ -41,7 +79,9 @@ class PortfeljService():
             "donos" : 8.49,             # vrednost - cena
             "trend" : 0.19              # za koliko procentov se je povecala vrednost, če pogledamo zadnja dva (razlicna?) vnosa
         }'''
-        return self.repo.dobi_portfelj(id_portfelja)
+        ids = [id_portfelja]
+        portfeljDto = self.posodobi_portfelje(ids)[0]
+        return portfeljDto
 
 
     def najdi_kriptovaluto(id_portfelja, id_kriptovalute):
