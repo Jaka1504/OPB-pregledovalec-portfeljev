23499df6e36b3f7685697f61fa163c52dd0b9b59
Tadej Jeršin <104827264+tadejjersin@users.noreply.github.com>
2024-05-26 21:01:48 +0200

    dodal možnost ustvarjanja portfelja

--- Services/portfelj_service.py
+++ Services/portfelj_service.py
@@ -42,20 +42,20 @@ class PortfeljService():
             id = portfelj.id,
             lastnik = portfelj.lastnik,
             ime = portfelj.ime,
-            vrednost = round(vrednosti[i], 2)
+            vrednost = round(vrednosti[i], 2),
+            kriptovalute=seznam_kolicin[i]
         ) for i, portfelj in enumerate(portfelji)]
 
 
     def najdi_vse_portfelje(self, uporabnisko_ime):
-        '''Vrne seznam portfeljev uporabnika. Vsak portfelj je slovar oblike
-        portfelj = {
-            "id" : 1,
-            "ime" : "kripto:)",
-            "cena" : 2.13,              # vsota cen vseh transakcij tega portfelja, koliko je uporabnik plačal
-            "vrednost" : 103.14,        # vsota (trenutna vrednost enote * kolicina) za vse transakcije
-            "donos" : 101.01,           # razlika prejsnjih dveh
-            "trend" : 0.4               # za koliko procentov se je povecala vrednost, če pogledamo zadnja dva (razlicna?) vnosa
-        }'''
+        '''Vrne seznam portfeljev uporabnika. Vsak portfelj objekt PortfeljDto() iz Data.modeli z atributi
+            id : 1,
+            ime : "kripto:)",
+            lastnik : "test",         # lastnik portfelja
+            vrednost : 103.14,        # vsota (trenutna vrednost enote * kolicina) za vse transakcije
+            kriptovalute : dict()     # slovar {id_kripto: kolicina_kripto}
+            Da dobiš objekt Kriptovaluta() iz id_kripto glej Services.kriptovalute_service (dobi_kriptovaluto).
+        '''
         portfelji = self.repo.dobi_uporabnikove_portfelje(uporabnisko_ime)
         ids = [p.id for p in portfelji]
         portfeljiDto = self.posodobi_portfelje(ids)
@@ -63,46 +63,24 @@ class PortfeljService():
 
 
     def najdi_portfelj(self, id_portfelja):
-        '''Vrne portfelj z ustreznim ID-jem. Portfelj naj bo slovar oblike 
-        portfelj = {
-            "id" : 1,
-            "ime" : "kripto:)",
-            "kriptovalute" : [kriptovaluta1, kriptovaluta2]
-        Kriptovalute naj bodo slovarji oblike
-        kriptovaluta1 = {
-            "id" : 1,
-            "ime" : "Bitcoin",
-            "kratica" : "BTC",
-            "cena" : 105.20,            # vsota cen vseh transakcij pri tem portfelju s to kriptovaluto
-            "kolicina" : 0.002,         # vsota kolicin vseh transakcij pri tem portfelju s to kriptovaluto
-            "vrednost": 113.69,         # trenutna vrednost enote * kolicina
-            "donos" : 8.49,             # vrednost - cena
-            "trend" : 0.19              # za koliko procentov se je povecala vrednost, če pogledamo zadnja dva (razlicna?) vnosa
-        }'''
+        '''Vrne portfelj z id-jem id_portfelja. Portfelj je objekt PortfeljDto() iz Data.modeli z atributi
+            id : 1,
+            ime : "kripto:)",
+            lastnik : "test",         # lastnik portfelja
+            vrednost : 103.14,        # vsota (trenutna vrednost enote * kolicina) za vse transakcije
+            kriptovalute : dict().     # slovar {id_kripto: kolicina_kripto}
+            Da dobiš objekt Kriptovaluta() iz id_kripto glej Services.kriptovalute_service (dobi_kriptovaluto).
+        '''
         ids = [id_portfelja]
         portfeljDto = self.posodobi_portfelje(ids)[0]
         return portfeljDto
 
 
-    def najdi_kriptovaluto(id_portfelja, id_kriptovalute):
-        '''Vrne naslednje podatke za dano kriptovaluto v danem portfelju v obliki slovarja:
-        kriptovaluta = {
-            "ime": "Bitcoin",
-            "kratica": "BTC",
-            "ime_portfelja": "kripto:)",
-            "vrednost_enote": 18697.65,         # trenutna vrednost enote kriptovalute
-            "trend": 0.64,                      # za koliko procentov se je povecala vrednost, če pogledamo zadnja dva (razlicna?) vnosa
-            "transakcije": [                    # seznam transakcij v tem portfelju s to kriptovaluto.
-                transakcija1,
-                transakcija2
-                ]
-            }
-        kjer so transakcije slovarji oblike:
-        transakcija1 = {
-            "id" : 1,
-            "cena_enote" : 20105.20,            # Takratna cena ene enote
-            "kolicina" : 0.002,
-            "datum" : "13. 5. 2024",
-            }
-        '''
-        pass
+    def ustvari_portfelj(self, uporabnisko_ime, ime_portfelja):
+        portfelj = Portfelj(
+            lastnik=uporabnisko_ime,
+            ime=ime_portfelja
+        )
+        self.repo.dodaj_portfelj(portfelj)
+        return portfelj
+
