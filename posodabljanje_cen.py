03608cdeca82a21652ee653e73be724ad804f528
Jaka Vrhovnik <jaka.vrhovnik3@gmail.com>
2024-06-08 13:21:08 +0200

    Popravil skripto za posodabljanje cen

--- posodabljanje_cen.py
+++ posodabljanje_cen.py
@@ -4,28 +4,37 @@ import schedule
 
 from Data.modeli import CenaKriptovalute
 from Data.database import Repo
+from Data.api import Api
 
 
 def posodobi_cene():
+    print(f"\n\n{datetime.now()} Začenjam posodobitev cen.")
     repo = Repo()
+    api = Api()
     kriptovalute = repo.dobi_kriptovalute()
-    for i, kripto in enumerate(kriptovalute):
-        try:
-            cenaKriptovalute = CenaKriptovalute(
-                kriptovaluta = kripto.id,
-                cas = datetime.now(),
-                cena = kripto.zadnja_cena
-            )
-            repo.posodobi_ceno_kriptovalute(kripto.id, kripto.zadnja_cena, kripto.trend24h, kripto.trend7d)
-            repo.dodaj_ceno_kriptovalute(cenaKriptovalute)
-            if (i + 1) % 100 != 0:
-                print(f"{datetime.now()} Posodobil {i} / {len(kriptovalute)}", end="\r")
-            else:
-                print(f"{datetime.now()} Posodobil {i} / {len(kriptovalute)}", end="\n")
-        except:
-            pass
+    print(f"\n\n{datetime.now()} Pobral id-je kriptovalut iz baze.")
+    ids = [kripto.id for kripto in kriptovalute]
+    kriptovalute, cas = api.dobi_cene_kriptovalut(ids)
+    print(f"\n\n{datetime.now()} Dobil podatke preko API-ja.")
+    for i, id in enumerate(ids):
+        kripto = kriptovalute[i]
+        # try:
+        cenaKriptovalute = CenaKriptovalute(
+            kriptovaluta = id,
+            cas = cas,
+            cena = kripto.zadnja_cena
+        )
+        repo.posodobi_ceno_kriptovalute(id, kripto.zadnja_cena, kripto.trend24h, kripto.trend7d)
+        repo.dodaj_ceno_kriptovalute(cenaKriptovalute)
+        if (i + 1) % 100 != 0:
+            print(f"{datetime.now()} Posodobil {i + 1} / {len(kriptovalute)}", end="\r")
+        else:
+            print(f"{datetime.now()} Posodobil {i + 1} / {len(kriptovalute)}", end="\n")
+        # except:
+        #     pass
 
-    print(f"{datetime.now()} Posodobil cene")
+    print(f"{datetime.now()} Končano posodabljanje cen.\n\n")
+    print("=====================================================")
 
 
 def test_task():
